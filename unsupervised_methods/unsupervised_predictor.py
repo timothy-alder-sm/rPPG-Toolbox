225eef3c7513688e16f052143033a5a0b4f79612
koutsd <koutsakis.d@hotmail.com>
2024-03-29 12:30:48 +0100

    added save outputs feature to unsupervised methods

--- unsupervised_methods/unsupervised_predictor.py
+++ unsupervised_methods/unsupervised_predictor.py
@@ -11,6 +11,33 @@ from unsupervised_methods.methods.OMIT import *
 from tqdm import tqdm
 from evaluation.BlandAltmanPy import BlandAltman
 
+import os
+import pickle
+import torch
+
+
+def save_test_outputs(predictions, labels, config, method_name):
+
+    output_dir = config.UNSUPERVISED.OUTPUT_SAVE_DIR
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+    
+    filename_id = method_name + "_" + config.UNSUPERVISED.DATA.DATASET
+
+    output_path = os.path.join(output_dir, filename_id + '_outputs.pickle')
+
+    data = dict()
+    data['predictions'] = predictions
+    data['labels'] = labels
+    data['label_type'] = config.UNSUPERVISED.DATA.PREPROCESS.LABEL_TYPE
+    data['fs'] = config.UNSUPERVISED.DATA.FS
+
+    with open(output_path, 'wb') as handle: # save out frame dict pickle file
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    print('Saving outputs to:', output_path)
+    print('')
+
 
 def unsupervised_predict(config, data_loader, method_name):
     """ Model evaluation on the testing dataset."""
@@ -21,6 +48,9 @@ def unsupervised_predict(config, data_loader, method_name):
     predict_hr_all = []
     gt_hr_all = []
     SNR_all = []
+    
+    predictions = dict()
+    labels = dict()
     MACC_all = []
     sbar = tqdm(data_loader["unsupervised"], ncols=80)
     for _, test_batch in enumerate(sbar):
@@ -44,6 +74,20 @@ def unsupervised_predict(config, data_loader, method_name):
                 BVP = OMIT(data_input)
             else:
                 raise ValueError("unsupervised method name wrong!")
+            
+            subj_index = test_batch[2][idx]
+            sort_index = int(test_batch[3][idx])
+            if subj_index not in predictions.keys():
+                predictions[subj_index] = dict()
+                labels[subj_index] = dict()
+                
+            if config.UNSUPERVISED.DATA.PREPROCESS.DO_CHUNK:
+                chunk_len = config.UNSUPERVISED.DATA.PREPROCESS.CHUNK_LENGTH
+                predictions[subj_index][sort_index] = torch.from_numpy(BVP[idx * chunk_len:(idx + 1) * chunk_len].copy())
+                labels[subj_index][sort_index] = torch.from_numpy(labels_input[idx * chunk_len:(idx + 1) * chunk_len].copy())
+            else:
+                predictions[subj_index][sort_index] = torch.from_numpy(BVP.copy())
+                labels[subj_index][sort_index] = torch.from_numpy(labels_input.copy())
             
             video_frame_size = test_batch[0].shape[1]
             if config.INFERENCE.EVALUATION_WINDOW.USE_SMALLER_WINDOW:
@@ -198,3 +242,56 @@ def unsupervised_predict(config, data_loader, method_name):
                 raise ValueError("Wrong Test Metric Type")
     else:
         raise ValueError("Inference evaluation method name wrong!")
+
+
+    predict_hr_all = np.array(predict_hr_all)
+    gt_hr_all = np.array(gt_hr_all)
+    SNR_all = np.array(SNR_all)
+    num_test_samples = len(predict_hr_all)
+    
+    for metric in config.UNSUPERVISED.METRICS:
+        if metric == "MAE":
+            MAE = np.mean(np.abs(predict_hr_all - gt_hr_all))
+            standard_error = np.std(np.abs(predict_hr_all - gt_hr_all)) / np.sqrt(num_test_samples)
+            print("MAE: {0} +/- {1}".format(MAE, standard_error))
+            # print(MAE)
+        elif metric == "RMSE":
+            RMSE = np.sqrt(np.mean(np.square(predict_hr_all - gt_hr_all)))
+            standard_error = np.std(np.square(predict_hr_all - gt_hr_all)) / np.sqrt(num_test_samples)
+            print("RMSE: {0} +/- {1}".format(RMSE, standard_error))
+            # print(RMSE)
+        elif metric == "MAPE":
+            MAPE = np.mean(np.abs((predict_hr_all - gt_hr_all) / gt_hr_all)) * 100
+            standard_error = np.std(np.abs((predict_hr_all - gt_hr_all) / gt_hr_all)) / np.sqrt(num_test_samples) * 100
+            print("MAPE: {0} +/- {1}".format(MAPE, standard_error))
+            # print(MAPE)
+        elif metric == "Pearson":
+            Pearson = np.corrcoef(predict_hr_all, gt_hr_all)
+            correlation_coefficient = Pearson[0][1]
+            standard_error = np.sqrt((1 - correlation_coefficient**2) / (num_test_samples - 2))
+            print("Pearson: {0} +/- {1}".format(correlation_coefficient, standard_error))
+            # print(correlation_coefficient)
+        elif metric == "SNR":
+            SNR = np.mean(SNR_all)
+            standard_error = np.std(SNR_all) / np.sqrt(num_test_samples)
+            print("SNR: {0} +/- {1} (dB)".format(SNR, standard_error))
+            # print(SNR)
+        elif "BA" in metric:
+            compare = BlandAltman(gt_hr_all, predict_hr_all, config, averaged=True)
+            compare.scatter_plot(
+                x_label='GT PPG HR [bpm]',
+                y_label='rPPG HR [bpm]',
+                show_legend=True, figure_size=(5, 5),
+                the_title=f'{filename_id}_BlandAltman_ScatterPlot',
+                file_name=f'{filename_id}_BlandAltman_ScatterPlot.pdf')
+            compare.difference_plot(
+                x_label='Difference between rPPG HR and GT PPG HR [bpm]',
+                y_label='Average of rPPG HR and GT PPG HR [bpm]',
+                show_legend=True, figure_size=(5, 5),
+                the_title=f'{filename_id}_BlandAltman_DifferencePlot',
+                file_name=f'{filename_id}_BlandAltman_DifferencePlot.pdf')
+        else:
+            raise ValueError("Wrong Test Metric Type")
+        
+    save_test_outputs(predictions, labels, config, method_name)
+    
